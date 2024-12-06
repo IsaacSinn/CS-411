f455fccea9cf96b884831251f5a1c4b26d804507
George Audi <gaudi@bu.edu>
2024-12-06 13:53:14 -0500

    added create_db

--- movie_recommender/models/user.py
+++ movie_recommender/models/user.py
@@ -7,6 +7,6 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String, unique=True, nullable=False)
+    username = Column(String, nullable=False, unique=True)
     salt = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
