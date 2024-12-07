d847117767b674a1ae8a9b3e825f51d61c562464
George Audi <gaudi@bu.edu>
2024-12-07 16:42:33 -0500

    smoketest

--- movie_recommender/smoketest.sh
+++ movie_recommender/smoketest.sh
@@ -34,5 +34,46 @@ check_health() {
   fi
 }
 
+##############################################
+#
+# User management
+#
+##############################################
+
+# Function to create a user
+create_account() {
+  echo "Creating a new account..."
+  curl -s -X POST "$BASE_URL/create-account" -H "Content-Type: application/json" \
+    -d '{"username":"testuser", "password":"password123"}' | grep -q '"message": "Account created sucessfully."'
+  if [ $? -eq 0 ]; then
+    echo "account created successfully."
+  else
+    echo "Failed to create account."
+    exit 1
+  fi
+}
+
+# Function to log in a user
+login_user() {
+  echo "Logging in user..."
+  response=$(curl -s -X POST "$BASE_URL/login" -H "Content-Type: application/json" \
+    -d '{"username":"testuser", "password":"password123"}')
+  if echo "$response" | grep -q '"message": "User testuser logged in successfully."'; then
+    echo "User logged in successfully."
+    if [ "$ECHO_JSON" = true ]; then
+      echo "Login Response JSON:"
+      echo "$response" | jq .
+    fi
+  else
+    echo "Failed to log in user."
+    if [ "$ECHO_JSON" = true ]; then
+      echo "Error Response JSON:"
+      echo "$response" | jq .
+    fi
+    exit 1
+  fi
+}
+
 # Run all the steps in order
 check_health
+create_account
